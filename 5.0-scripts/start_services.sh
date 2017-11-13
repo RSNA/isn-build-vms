c3c2efa38539fb0a3c779cc8374e0f1602033fad
Steve Moore <moores@mir.wustl.edu>
2017-11-13 14:26:30 -0600

    Move the command to start postgres to the front of the list of services. This needs to happen before we start the apps that depend on postgres.

--- 5.0-scripts/start_services.sh
+++ 5.0-scripts/start_services.sh
@@ -6,7 +6,8 @@ MIRTH=/usr/local/mirthconnect
 
 $SUDO ls /tmp
 
+$SUDO systemctl start postgresql
+sleep 5
 $SUDO systemctl start torquebox
 $SUDO systemctl start edge-server
 $SUDO systemctl start mirthconnect
-$SUDO systemctl start postgresql
